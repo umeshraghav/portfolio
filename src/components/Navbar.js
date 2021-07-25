699688534ebcff72e0b20e016fe362a8f565e0e6
Umesh Raghav <umeshraghav55@gmail.com>
2021-07-25 18:09:27 +0530

    Added Toggle Functionality in nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState,useRef, useEffect} from 'react';
 import logo from '../logo.svg';
 import {FaBars} from 'react-icons/fa'
 import {links, socialLinks} from '../data'
@@ -6,16 +6,30 @@ import {links, socialLinks} from '../data'
 
 
 export default function Navbar() {
+    const [showLinks, setShowLinks] = useState(false)    
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null);
+    useEffect(() => {
+        const linksHeight = linksRef.current.getBoundingClientRect().height;
+        if(showLinks){
+            linksContainerRef.current.style.height=`${linksHeight}px`;
+        }else{
+            linksContainerRef.current.style.height = '0px'
+        }
+
+
+    }, [showLinks])
+
     return (
         <nav>
                   <div className="nav-center">
                             <div className="nav-header">
                                         <img className="logo" src={logo} alt="Umesh Raghav" /> 
-                                        <button className='nav-toggle'><FaBars/></button>
+                                        <button className='nav-toggle' onClick={()=>setShowLinks(!showLinks)}><FaBars/></button>
                             </div>
 
-                            <div className="links-container show-conatiner">
-                                    <ul className="links">
+                            <div className="links-container" ref={linksContainerRef}>
+                                    <ul className="links" ref={linksRef}>
                                         {links.map((link)=>{
                                             return <li key={link.id}> 
                                             
@@ -23,7 +37,7 @@ export default function Navbar() {
                                               </li>
                                         })}
                                     </ul>
-                            </div>
+                                    </div>
                             <ul className="social-icons">
                                 {socialLinks.map((sl)=>{
                                         return <li key={sl.id}>
@@ -32,7 +46,7 @@ export default function Navbar() {
                                         </li>
                                 })}
                             </ul>
-                      
+                          
                       </div>  
         </nav>
     )
